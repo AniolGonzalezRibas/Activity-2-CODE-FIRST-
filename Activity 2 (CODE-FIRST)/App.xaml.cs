33ab33719f8b2f5aefd9eb7a78a273d51b3257c6
Aniol <a2208023@institutmontilivi.cat>
2024-02-19 16:27:08 +0100

    Agregar archivos de proyecto.

---
+++ Activity 2 (CODE-FIRST)/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Activity_2__CODE_FIRST_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
