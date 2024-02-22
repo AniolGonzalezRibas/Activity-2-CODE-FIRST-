d5b3c661057f9945177ce757d3925320f8ef35e9
ArnauSerra <a2208024@institutmontilivi.cat>
2024-02-22 16:44:46 +0100

    Add ClassicModelDbContext

---
+++ Activity 2 (CODE-FIRST)/MODEL/ClassicModelDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_2__CODE_FIRST_.MODEL
+{
+    public class ClassicModelDbContext : DbContext
+    {
+        public ClassicModelDbContext() { }
+
+        public ClassicModelDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("Server=localhost;Database=CLASSICMODEL;Uid=root;Pwd=\"\"");
+            }
+        }
+
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Office> Offices { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<Payment> Payments { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductLines> ProductLiness { get; set; }
+
+    }
+}
+ 
